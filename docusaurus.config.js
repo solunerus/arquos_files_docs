e941e7c8b741766447678139e4e3881a6730faec
OscarTinajero117 <tinajero117.oscar@gmail.com>
2025-01-31 20:18:14 -0600

    cambio de puerto

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://arquoscrm.sytes.net:4501/",
+  url: "https://arquoscrm.sytes.net:5501/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
